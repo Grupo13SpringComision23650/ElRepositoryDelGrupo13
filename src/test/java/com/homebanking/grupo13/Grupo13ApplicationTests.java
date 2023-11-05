0bde84cd0ad48ee5f1eaa4106ffb6e12dfe0dbb8
Luciano A <adm.red.2018@example.com>
2023-11-04 23:38:10 -0300

    Primer Commit

---
+++ src/test/java/com/homebanking/grupo13/Grupo13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homebanking.grupo13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
