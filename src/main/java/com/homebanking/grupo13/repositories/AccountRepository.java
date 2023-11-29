b3cf8326b72f3d88bdbe3520ac7ba3fa3e841b3c
Oscar Sebastián Báez <86858733+SeBaez85@users.noreply.github.com>
2023-11-29 19:23:59 -0300

    CRUD Account al 21-11 (#7)

---
+++ src/main/java/com/homebanking/grupo13/repositories/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.homebanking.grupo13.repositories;
+
+import com.homebanking.grupo13.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
